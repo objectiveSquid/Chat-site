9c05adc1d2c307f0212d53a9caa172c0f4749121
objectiveSquid <magnuskzahle@outlook.com>
2024-05-21 18:55:37 +0200

    this is prettier imo

--- shared/logger.py
+++ shared/logger.py
@@ -25,7 +25,7 @@ def configure_logger(level: int) -> None:
     console_handler = logging.StreamHandler()
     console_handler.setLevel(level)
     console_formatter = ColoredFormatter(
-        "%(asctime)s - %(levelname)s - %(message)s", datefmt="%H:%M:%S"
+        "%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s", datefmt="%H:%M:%S"
     )
     console_handler.setFormatter(console_formatter)
 
